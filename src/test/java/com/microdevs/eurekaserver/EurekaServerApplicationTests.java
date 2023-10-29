b932e6f440b9d167c446aa87ba96d3d7d67a0357
Muhammet Emre Vatan <emre.vatan98@hotmail.com>
2023-10-29 16:55:05 +0300

    init commit

---
+++ src/test/java/com/microdevs/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microdevs.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
